995b620536060c34bf294697af4718ddfb9b665a
Andre <andre.isaac.jessee@gmail.com>
2020-06-17 18:21:18 -0500

    refactor

--- lib/table_info.rb
+++ lib/table_info.rb
@@ -1,31 +1,13 @@
 module TableInfo
   extend self
-
-  if Rails.env.production?
-    if defined?(ActiveRecord::Base)
-      ActiveRecord::Base.establish_connection
-      DB = ActiveRecord::Base.connection
-    end
-  else
-    DB = ActiveRecord::Base.connection
-  end 
   
-
   def schema_for(table_name)
 
-    if Rails.env.production?
-      DB.reconnect!
-    end
-    
     metadata = {table_name: table_name}
     
-    DB.transaction do
-      metadata[:fields] = DB.execute("SELECT * FROM #{table_name} WHERE false").fields
+    ActiveRecord::Base.connection.transaction do
+      metadata[:fields] = ActiveRecord::Base.connection.execute("SELECT * FROM #{table_name} WHERE false").fields
       raise ActiveRecord::Rollback
-    end
-
-    if Rails.env.production?
-      ActiveRecord::Base.connection.disconnect!
     end
     
     metadata
