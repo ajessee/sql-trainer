bf1738807f362185e9c261d2b13d26525cfb5c20
Andre <andre.isaac.jessee@gmail.com>
2020-06-17 18:09:54 -0500

    trying to do magic now with db connection in prod

--- lib/table_info.rb
+++ lib/table_info.rb
@@ -1,19 +1,33 @@
 module TableInfo
   extend self
 
-  @@DB = ActiveRecord::Base.connection
+  if Rails.env.production?
+    if defined?(ActiveRecord::Base)
+      ActiveRecord::Base.establish_connection
+      DB = ActiveRecord::Base.connection
+    end
+  else
+    DB = ActiveRecord::Base.connection
+  end 
+  
 
   def schema_for(table_name)
+
+    if Rails.env.production?
+      DB.reconnect!
+    end
+    
     metadata = {table_name: table_name}
     
-    if Rails.env.production?
-      @@DB.reconnect!
-    end
-    @@DB.transaction do
-      metadata[:fields] = @@DB.execute("SELECT * FROM #{table_name} WHERE false").fields
+    DB.transaction do
+      metadata[:fields] = DB.execute("SELECT * FROM #{table_name} WHERE false").fields
       raise ActiveRecord::Rollback
     end
 
+    if Rails.env.production?
+      ActiveRecord::Base.connection.disconnect!
+    end
+    
     metadata
   end
 end
