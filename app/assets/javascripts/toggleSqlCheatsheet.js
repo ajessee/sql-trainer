4a9954ffc16124f4efb046d1df040d082688210d
Andre <andre.isaac.jessee@gmail.com>
2020-06-17 22:29:56 -0500

    i guess i can't use es6 with uglifier

--- app/assets/javascripts/toggleSqlCheatsheet.js
+++ app/assets/javascripts/toggleSqlCheatsheet.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(e){
-    const toggleCheatSheet = document.querySelector('#toggle-cheat-sheet');
-    const cheatSheet = document.querySelector('#sql-cheat-sheet');
+    var toggleCheatSheet = document.querySelector('#toggle-cheat-sheet');
+    var cheatSheet = document.querySelector('#sql-cheat-sheet');
     cheatSheet.style.display = "none";
     
     toggleCheatSheet.addEventListener('click', function(e){
