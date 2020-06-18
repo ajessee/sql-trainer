2f37ed52b45a8b6c2a847e2a21e7e3f5256b3fdc
Andre <andre.isaac.jessee@gmail.com>
2020-06-17 21:12:03 -0500

    update table info module with conditionals for envs

--- lib/table_info.rb
+++ lib/table_info.rb
@@ -1,13 +1,25 @@
 module TableInfo
   extend self
+
+  if Rails.env.development?
+    DB = ActiveRecord::Base.connection
+  end
   
   def schema_for(table_name)
-
+    
     metadata = {table_name: table_name}
     
-    ActiveRecord::Base.connection.transaction do
-      metadata[:fields] = ActiveRecord::Base.connection.execute("SELECT * FROM #{table_name} WHERE false").fields
-      raise ActiveRecord::Rollback
+    # for prod on heroku, call ActiveRecord::Base.connection directly or you get an error
+    if Rails.env.production?
+      ActiveRecord::Base.connection.transaction do
+        metadata[:fields] = ActiveRecord::Base.connection.execute("SELECT * FROM #{table_name} WHERE false").fields
+        raise ActiveRecord::Rollback
+      end
+    else
+      DB.transaction do
+        metadata[:fields] = DB.execute("SELECT * FROM #{table_name} WHERE false").fields
+        raise ActiveRecord::Rollback
+      end
     end
     
     metadata
